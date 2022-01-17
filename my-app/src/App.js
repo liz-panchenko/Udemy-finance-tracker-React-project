332e3773680a0d8b1bb264ba82426e07ef87b2d2
Liza Panchenko <panchenkoliza@gmail.com>
2022-01-17 15:46:01 +0000

    ExpenseItem component added with CSS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello world!</p>
+        <ExpenseItem />
       </header>
     </div>
   );
