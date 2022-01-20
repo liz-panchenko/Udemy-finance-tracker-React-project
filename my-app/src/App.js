63725cfd1edf13eebd18b5fbdbb58394ffbdd365
Liza Panchenko <panchenko.elisa@gmail.com>
2022-01-20 12:01:48 +0000

    NewExpenses Form created

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
  const expenses = [
@@ -26,7 +27,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello world!</p>
+        <NewExpense />
         <Expenses expenses={expenses}/>
       </header>
     </div>
