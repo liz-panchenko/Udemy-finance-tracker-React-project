60394a45b90c656ee12010cff0842684f5ab576d
Liza Panchenko <panchenko.elisa@gmail.com>
2022-01-21 17:31:22 +0000

    ExpenseItem component state removed

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -7,21 +6,14 @@ export default function ExpenseItem(props) {
   {
     /* <div>{props.date.toISOString()}</div> */
   }
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
