6a3de0144dc37c9620dbd02d176bf52f3f985e2e
Liza Panchenko <panchenko.elisa@gmail.com>
2022-01-21 15:48:35 +0000

    Entered expense data passed to App.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-export default function ExpenseForm() {
+export default function ExpenseForm(props) {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -47,10 +47,10 @@ export default function ExpenseForm() {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
-    console.log(expenseData);
   };
   return (
     <form onSubmit={submitHandler}>
